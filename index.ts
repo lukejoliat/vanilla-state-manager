c34ae247ab6f91431ace146a434a8e56844d4ecc
luke.joliat <luke.joliat@slalom.com>
2022-05-19 23:41:31 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,139 @@
+export type State = object;
+export type PartialState<T extends State> =
+  | Partial<T>
+  | ((state: T) => Partial<T>);
+export type StateSelector<T extends State, U> = (state: T) => U;
+export type EqualityChecker<T> = (state: T, newState: T) => boolean;
+export type StateListener<T> = (state: T, previousState: T) => void;
+export type StateGetter<T> = (state: T) => T[keyof T];
+/**
+ * @deprecated Use `StateListener<T>` instead of `StateSliceListener<T>`.
+ */
+export type StateSliceListener<T> = (slice: T, previousSlice: T) => void;
+export type Subscribe<T extends State> = {
+  (listener: StateListener<T>, getter?: any): () => void;
+};
+
+export type SetState<T extends State> = {
+  _(
+    partial: T | Partial<T> | ((state: T) => T | Partial<T>),
+    replace?: boolean | undefined
+  ): void;
+}["_"];
+export type GetState<T extends State> = () => T;
+export type Destroy = () => void;
+export type StoreApi<T extends State> = {
+  setState: SetState<T>;
+  getState: GetState<T>;
+  subscribe: Subscribe<T>;
+  destroy: Destroy;
+};
+
+export type StateCreator<
+  T extends State,
+  Mis extends [StoreMutatorIdentifier, unknown][] = [],
+  Mos extends [StoreMutatorIdentifier, unknown][] = [],
+  U = T
+> = ((
+  setState: Get<Mutate<StoreApi<T>, Mis>, "setState", undefined>,
+  getState: Get<Mutate<StoreApi<T>, Mis>, "getState", undefined>,
+  store: Mutate<StoreApi<T>, Mis>,
+  $$storeMutations: Mis
+) => U) & { $$storeMutators?: Mos };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export interface StoreMutators<S, A> {}
+export type StoreMutatorIdentifier = keyof StoreMutators<unknown, unknown>;
+
+export type Mutate<S, Ms> = Ms extends []
+  ? S
+  : Ms extends [[infer Mi, infer Ma], ...(infer Mrs)]
+  ? Mutate<StoreMutators<S, Ma>[Mi & StoreMutatorIdentifier], Mrs>
+  : never;
+
+type Get<T, K, F = never> = K extends keyof T ? T[K] : F;
+
+type CreateStore = {
+  <T extends State, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
+    initializer: StateCreator<T, [], Mos>
+  ): Mutate<StoreApi<T>, Mos>;
+
+  <T extends State>(): <Mos extends [StoreMutatorIdentifier, unknown][] = []>(
+    initializer: StateCreator<T, [], Mos>
+  ) => Mutate<StoreApi<T>, Mos>;
+};
+
+type CreateStoreImpl = <
+  T extends State,
+  Mos extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  initializer: StateCreator<T, [], Mos>
+) => Mutate<StoreApi<T>, Mos>;
+
+type PopArgument<T extends (...a: never[]) => unknown> = T extends (
+  ...a: [...(infer A), infer _]
+) => infer R
+  ? (...a: A) => R
+  : never;
+
+const createStoreImpl: CreateStoreImpl = (createState) => {
+  type TState = ReturnType<typeof createState>;
+  let state: TState;
+  const listeners: Set<{
+    callback: StateListener<TState>;
+    getter: StateGetter<TState>;
+  }> = new Set();
+
+  const setState: SetState<TState> = (partial, replace) => {
+    // TODO: Remove type assertion once https://github.com/microsoft/TypeScript/issues/37663 is resolved
+    // https://github.com/microsoft/TypeScript/issues/37663#issuecomment-759728342
+    const nextState =
+      typeof partial === "function"
+        ? (partial as (state: TState) => TState)(state)
+        : partial;
+    if (nextState !== state) {
+      const previousState = state;
+      state = replace
+        ? (nextState as TState)
+        : Object.assign({}, state, nextState);
+
+      listeners.forEach((listener) => {
+        if (
+          listener.getter &&
+          listener.getter(state) !== listener.getter(previousState)
+        ) {
+          listener.callback(state, previousState);
+        }
+        if (!listener.getter) {
+          listener.callback(state, previousState);
+        }
+      });
+    }
+  };
+
+  const getState: GetState<TState> = () => state;
+
+  const subscribe: Subscribe<TState> = (
+    callback: StateListener<TState>,
+    getter: StateGetter<TState>
+  ) => {
+    const listener = { callback, getter };
+    listeners.add(listener);
+    // Unsubscribe
+    return () => listeners.delete(listener);
+  };
+
+  const destroy: Destroy = () => listeners.clear();
+  const api = { setState, getState, subscribe, destroy };
+  state = (createState as PopArgument<typeof createState>)(
+    setState,
+    getState,
+    api
+  );
+  return api as any;
+};
+
+const createStore = ((createState) =>
+  createState ? createStoreImpl(createState) : createStoreImpl) as CreateStore;
+
+export default createStore;
